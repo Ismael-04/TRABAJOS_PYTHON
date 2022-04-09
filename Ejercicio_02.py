2caf4f283bc9d897eea5495e725857711317dd27
ISMAEL <al_igarciae@iessierradelasvillas.org>
2022-04-09 22:46:57 +0200

    2

---
+++ Ejercicio_02.py
@@ -0,0 +1,21 @@
+intentos=0
+
+numero=random.randint(1,100)
+
+while intentos<10
+    print('Intenta adivinar el numero que estoy pensando entre el 1 y el 100')
+    numintro= input()
+    numintro=int(numintro)
+
+    intentos=intentos+
+
+    if numintro>numero
+        print('El numero introducido es mayor al que estoy pensando.')
+
+    if numintro<numero
+        print('El numero introducido es menor al que estoy pensando.')
+    
+    if numintro==numero
+        break
+
+if numintro==numero

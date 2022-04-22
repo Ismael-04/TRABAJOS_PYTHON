feb1209eb5e1e47fb959e68407fee21f798e8bc0
Ismael <al_igarciae@iessierradelasvillas.org>
2022-04-22 13:02:10 +0200

    Ejercicio 1

--- Ejercicio_01.py
+++ Ejercicio_01.py
@@ -2,8 +2,12 @@
 #número es el producto de todos los enteros entre 1 y el propio número y se
 #representa por el número seguido de un signo de exclamación. Por ejemplo 5! =
 #1x2x3x4x5=120),
-numero=(int)(input("Introduzca un numero "))
-fact=1
-for i in range(2,numero+1):
-	fact*=i
-print("El resultado es ", fact)
+factorial=1
+print("Hola")
+numero=(int)(input("Introduzca el número del que quiere saber su factorial:..."))
+if (numero<0):
+    print("No existe el factorial de un número negativo")
+else:
+    for i in range(2,(numero+1)):
+        factorial= factorial*i
+print("El factorial de ", numero, " es ", factorial)
